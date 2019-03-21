a060923e6cb7eb762d0a2d3465084fbdb0c4efc8
Roberto <roby.belotti@gmail.com>
2019-03-21 13:37:25 +0100

    SensorValues created

---
+++ src/Values/SensorValues.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Copyright (c) 2019 - present
+ * Google Maps PHP - SensorValues.php
+ * author: Roberto Belotti - roby.belotti@gmail.com
+ * web : robertobelotti.com, github.com/biscolab
+ * Initial version created on: 21/3/2019
+ * MIT license: https://github.com/biscolab/google-maps-php/blob/master/LICENSE
+ */
+
+namespace Biscolab\GoogleMaps\Values;
+
+/**
+ * Class SensorValues
+ * @package Biscolab\GoogleMaps\Values
+ */
+class SensorValues
+{
+
+	/**
+	 * string: true
+	 */
+	const TRUE = 'true';
+
+	/**
+	 * string: false
+	 */
+	const FALSE = 'false';
+
+}
