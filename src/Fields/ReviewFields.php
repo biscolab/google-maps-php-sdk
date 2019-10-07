24b8991384f64bf97e88fe08e981b2ddec5b11a3
Roberto <roby.belotti@gmail.com>
2019-10-07 22:32:06 +0200

    Place details added

---
+++ src/Fields/ReviewFields.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * Copyright (c) 2019 - present
+ * Google Maps PHP - ReviewFields.php
+ * author: Roberto Belotti - roby.belotti@gmail.com
+ * web : robertobelotti.com, github.com/biscolab
+ * Initial version created on: 12/9/2019
+ * MIT license: https://github.com/biscolab/google-maps-php/blob/master/LICENSE
+ */
+
+namespace Biscolab\GoogleMaps\Fields;
+
+/**
+ * Class ReviewFields
+ * @package Biscolab\GoogleMaps\Fields
+ * @since v0.6.0
+ */
+class ReviewFields
+{
+
+	/**
+	 * @var string
+	 */
+	const AUTHOR_NAME = "author_name";
+
+	/**
+	 * @var string
+	 */
+	const AUTHOR_URL = "author_url";
+
+	/**
+	 * @var string
+	 */
+	const LANGUAGE = "language";
+
+	/**
+	 * @var string
+	 */
+	const PROFILE_PHOTO_URL = "profile_photo_url";
+
+	/**
+	 * @var string
+	 */
+	const RATING = "rating";
+
+	/**
+	 * @var string
+	 */
+	const RELATIVE_TIME_DESCRIPTION = "relative_time_description";
+
+	/**
+	 * @var string
+	 */
+	const TEXT = "text";
+
+	/**
+	 * @var string
+	 */
+	const TIME = "time";
+}
